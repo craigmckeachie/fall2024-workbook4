272cb3d732bc7a41262088c55c6fedaf6a39fbea
Craig McKeachie <craigdmckeachie@gmail.com>
2024-10-21 13:37:32 -0400

    label maker solutions

--- exercises/label_maker.js
+++ exercises/label_maker.js
@@ -1,12 +1,34 @@
 "use strict";
 
-let contact = {
+let jamesBond = {
   name: "James Bond",
   address: "125 Rue Way",
   city: "New York",
   state: "New York",
   zip: 10035,
 };
+let maryPoppins = {
+  name: "Mary Poppins",
+  address: "125th Ave",
+  city: "Powell",
+  state: "Ohio",
+  zip: 43065,
+};
+
+function printLabel(contact1, contact2) {
+  console.log(contact1, contact2);
+}
+
+printLabel(maryPoppins, jamesBond);
+
+// function printLabel(contact) {
+//   let mailingLabel = `
+//        ${contact.name}
+//        ${contact.address}
+//        ${contact.city}, ${contact.state} ${contact.zip}
+//       `;
+//   console.log(mailingLabel);
+// }
 
 // function printLabel(contact) {
 //   console.log(contact.name);
@@ -14,14 +36,3 @@ let contact = {
 //   console.log(contact.city + ",", contact.state, contact.zip);
 //   console.log(contact.city + ", " + contact.state + " " + contact.zip);
 // }
-
-function printLabel(contact) {
-  let mailingLabel = ` 
-   ${contact.name}
-   ${contact.address}
-   ${contact.city}, ${contact.state} ${contact.zip}
-  `;
-  console.log(mailingLabel);
-}
-
-printLabel(contact);
