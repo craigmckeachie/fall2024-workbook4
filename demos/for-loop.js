3331b972e201aabddf48e2f6f10043e0ab32264d
Craig McKeachie <craigdmckeachie@gmail.com>
2024-10-21 14:04:30 -0400

    for loop start

---
+++ demos/for-loop.js
@@ -0,0 +1,12 @@
+"use strict";
+
+//1. code that executes before the loop begins
+//2. a condition that must be true for the loop to keep executing
+//3. code that runs at the bottom of each iteration
+
+let count = 1;
+
+while (count <= 100) {
+  console.log("hi " + count);
+  count = count + 1; // or you could do count++
+}
