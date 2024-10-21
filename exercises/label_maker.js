cb2bc6e611bb1eaa4cf10a6b1a7f4f34adebf841
Craig McKeachie <craigdmckeachie@gmail.com>
2024-10-21 13:20:32 -0400

    label maker

--- exercises/label_maker.js
+++ exercises/label_maker.js
@@ -1,3 +1,27 @@
 "use strict";
 
+let contact = {
+  name: "James Bond",
+  address: "125 Rue Way",
+  city: "New York",
+  state: "New York",
+  zip: 10035,
+};
 
+// function printLabel(contact) {
+//   console.log(contact.name);
+//   console.log(contact.address);
+//   console.log(contact.city + ",", contact.state, contact.zip);
+//   console.log(contact.city + ", " + contact.state + " " + contact.zip);
+// }
+
+function printLabel(contact) {
+  let mailingLabel = ` 
+   ${contact.name}
+   ${contact.address}
+   ${contact.city}, ${contact.state} ${contact.zip}
+  `;
+  console.log(mailingLabel);
+}
+
+printLabel(contact);
