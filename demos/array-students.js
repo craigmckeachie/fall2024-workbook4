9c9539e39b1348cc48072651d103fa0355482f7c
Craig McKeachie <craigdmckeachie@gmail.com>
2024-10-21 15:44:32 -0400

    array students

---
+++ demos/array-students.js
@@ -0,0 +1,8 @@
+// let student = {}; //object, single thing
+let students = ["Gurgot", "Melina", "Kameron", "Timothy", "Christopher"]; //array, list, many things
+
+// console.log(students[4]);
+
+for (let index = 0; index < 5; index = index + 1) {
+  console.log(students[index]);
+}
