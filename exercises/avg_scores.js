f8ae6acd57b27afd58ac9fafd278b94f976dbbf9
Craig McKeachie <craigdmckeachie@gmail.com>
2024-10-22 11:08:52 -0400

    avg scores

---
+++ exercises/avg_scores.js
@@ -0,0 +1,15 @@
+"use strict";
+//arrays
+//functions, calling functions, passing parameters, catch return value in variable
+
+//for loop
+// operators to the summing
+//
+
+let list = [92, 98, 84, 76, 89, 99, 100];
+// let yourScores = [82, 98, 94, 88, 92, 100, 100];
+
+for (let position = 0; position < list.length; position++) {
+  const item = list[position];
+  console.log(item);
+}
