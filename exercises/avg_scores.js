263f14b6da7b0f4688a77054c6c58fc283e907d0
Craig McKeachie <craigdmckeachie@gmail.com>
2024-10-22 11:54:10 -0400

    avg scores

--- exercises/avg_scores.js
+++ exercises/avg_scores.js
@@ -1,20 +1,27 @@
 "use strict";
 
 let myScores = [92, 98, 84, 76, 89, 99, 100];
-// let yourScores = [82, 98, 94, 88, 92, 100, 100];
+let yourScores = [82, 98, 94, 88, 92, 100, 100];
+let williesScores = [93, 98, 94, 95, 99, 100, 100];
 
-function getAverage(myScores) {
+function getAverage(scores) {
   let sumOfAllScores = 0;
-  for (const score of myScores) {
+  for (const score of scores) {
     sumOfAllScores = sumOfAllScores + score; //sumOfAllScores += score;
   }
 
-  let averageScore = sumOfAllScores / myScores.length;
-  console.log(averageScore);
+  let averageScore = sumOfAllScores / scores.length;
   return averageScore;
 }
 
-getAverage(myScores)
+let myAverageScore = getAverage(myScores);
+console.log(myAverageScore.toFixed(2));
+
+let yourAverageScore = getAverage(yourScores);
+console.log(yourAverageScore.toFixed(2));
+
+let williesAverageScore = getAverage(williesScores);
+console.log(williesAverageScore);
 
 
 
