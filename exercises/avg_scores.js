57776940453ca9f764ec39a8e8151832cd64d7c7
Craig McKeachie <craigdmckeachie@gmail.com>
2024-10-22 11:21:09 -0400

    avg scores update

--- exercises/avg_scores.js
+++ exercises/avg_scores.js
@@ -11,5 +11,22 @@ let list = [92, 98, 84, 76, 89, 99, 100];
 
 for (let position = 0; position < list.length; position++) {
   const item = list[position];
+  //   console.log(item);
+  //do job
+}
+
+for (const item of list) {
   console.log(item);
+}
+
+let cats = ["Garfield", "Tom", "Toma", "Mochi", "Felix", "Flex", "Sylvester", "P Kitty"];
+
+for (const cat of cats) {
+  console.log(cat);
+}
+
+for (let index = 0; index < cats.length; index++) {
+  const cat = cats[index];
+  console.log(cat);
+  
 }
