887d8788a9fc6ddf2fed479b42ba0a63a5cf513d
Craig McKeachie <craigdmckeachie@gmail.com>
2024-10-22 11:30:46 -0400

    add up all the numbers

--- exercises/avg_scores.js
+++ exercises/avg_scores.js
@@ -6,27 +6,35 @@
 // operators to the summing
 //
 
-let list = [92, 98, 84, 76, 89, 99, 100];
+let myScores = [92, 98, 84, 76, 89, 99, 100];
 // let yourScores = [82, 98, 94, 88, 92, 100, 100];
 
-for (let position = 0; position < list.length; position++) {
-  const item = list[position];
-  //   console.log(item);
-  //do job
+let sumOfAllScores = 0;
+for (const score of myScores) {
+  console.log(score);
+  sumOfAllScores = sumOfAllScores + score; //sumOfAllScores += score;  
 }
+console.log(sumOfAllScores);
 
-for (const item of list) {
-  console.log(item);
-}
+// let list = [92, 98, 84, 76, 89, 99, 100];
+// for (let position = 0; position < list.length; position++) {
+//   const item = list[position];
+//   //   console.log(item);
+//   //do job
+// }
 
-let cats = ["Garfield", "Tom", "Toma", "Mochi", "Felix", "Flex", "Sylvester", "P Kitty"];
+// for (const item of list) {
+//   console.log(item);
+// }
 
-for (let cat of cats) {
-  console.log(cat);
-}
+// let cats = ["Garfield", "Tom", "Toma", "Mochi", "Felix", "Flex", "Sylvester", "P Kitty"];
 
-for (let index = 0; index < cats.length; index++) {
-  const cat = cats[index];
-  console.log(cat);
-  
-}
+// for (let cat of cats) {
+//   console.log(cat);
+// }
+
+// for (let index = 0; index < cats.length; index++) {
+//   const cat = cats[index];
+//   console.log(cat);
+
+// }
